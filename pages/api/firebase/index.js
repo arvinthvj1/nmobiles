86e6c1f166796491002531272fab252bc4bfce90
Arvinth <amanivannaboopathy@inniveinc.com>
2024-07-04 08:18:42 +0530

    update changes in categories

---
+++ pages/api/firebase/index.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+"use client"
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBcR4o8MtmfunMSDOi06nAIh8O8YVAXhtM",
+  authDomain: "batch5-32f82.firebaseapp.com",
+  projectId: "batch5-32f82",
+  storageBucket: "batch5-32f82.appspot.com",
+  messagingSenderId: "207585484889",
+  appId: "1:207585484889:web:dc9075ff00b0048851d148",
+  measurementId: "G-J31FL6J4J8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+export { auth, provider, signInWithPopup };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-import {nextui} from '@nextui-org/theme'
+import { nextui } from '@nextui-org/theme'
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -7,11 +7,36 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
     './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
   ],
-  theme: {
-    extend: {},
-  },
-  darkMode: "class",
+  darkMode: "class", // Enable dark mode using a class
   plugins: [nextui({
+    themes: {
+      dark: {
+        colors: {
+          primary: {
+            DEFAULT: "#FF00FF",
+            foreground: "#000000",
+          },
+          focus: "#BEF264",
+          danger: {
+            DEFAULT: "#BEF264",
+            foreground: "#000000",
+          },
+        },
+      },
+      light: {
+        colors: {
+          primary: {
+            DEFAULT: "#BEF264",
+            foreground: "#000000",
+          },
+          focus: "#BEF264",
+          danger: {
+            DEFAULT: "#BEF264",
+            foreground: "#000000",
+          },
+        },
+      },
+    },
     layout: {
       disabledOpacity: "0.3", // opacity-[0.3]
       radius: {
